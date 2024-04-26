e8c432bf3d6439b11c6ef06febda2cff4b123825
Michael Clark <michaelpclark03@gmail.com>
2024-04-25 21:32:16 -0500

    server initial working

--- server/main.py
+++ server/main.py
@@ -8,22 +8,19 @@ import shutil
 app = FastAPI()
 
 
+upload_directory = "./uploaded_images"
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 
 @app.post("/identify")
-async def identify(file: UploadFile):
-    file_location = f"uploaded_images/{file.filename}"
-
-    os.makedirs(os.path.dirname(file_location), exist_ok=True)
-
-    with open(file_location, "wb") as buffer:
+async def create_upload_file(file: UploadFile):
+    file_path = os.path.join(upload_directory, file.filename)
+    
+    # Save the uploaded file
+    with open(file_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
-
-    return {
-        "filename": file.filename,
-        "content_type": file.content_type,
-        "message": "Image uploaded successfully",
-    }
+    
+    return {"filename": file.filename, "path": file_path}
